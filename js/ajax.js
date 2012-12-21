098233c317a9ccb6e84aea8925b07a3f55faf9d8
Kevin Deamandel <kevin@Kevins-MacBook-Pro.local>
2012-12-21 15:23:02 +0100

    Inserting Site

---
+++ js/ajax.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+$(document).ready(function(){
+
+   // jQuery methods go here..
+   
+	//$('#content').load('ajax/contact.html #container');
+	$("#contact").click(function() {
+		$('#content').load('ajax/contact.html #container');});
+});
